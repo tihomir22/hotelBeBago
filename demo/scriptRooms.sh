edef8585d7aa5cdfa505692dc8767d7de9b3c6c2
Tihomir <tihomir_alcudia3@hotmail.com>
2018-11-02 21:58:56 +0100

    terminados demo ficheros

--- demo/scriptRooms.sh
+++ demo/scriptRooms.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-echo "<?xml version="1.0" encoding="utf-8"?>
+echo '<?xml version="1.0" encoding="utf-8"?>
 <odoo>
-	<data>"
+	<data>'
 i=0;
+arrayString=("desagradable" "majestuosa" "primordial" "agradable" "fabulosa" "denigrante" "marginal")
 
 	while [ $i -lt 400 ] ; do
-		echo "
-		<record model="hotels_be_bago.hotel" id="hotel1">
-			<field name="name">Hotel Number 9</field>
-			<field name="description">Extra dip.</field>
-			<!-- para Many2One-->
-			<field name="ciudad" ref="hotels_be_bago.ciudad1"></field>
-			<!-- para Many2Many -->
-
-          <field name="listaServicios" eval="[(6,0,[ref('hotels_be_bago.servici1'),ref('hotels_be_bago.servici2')] )]" />
-
-			<field name="valoraciones">5</field>
-			 <field name="listaServicios" eval="[(6,0,[ref('hotels_be_bago.servici1'),ref('hotels_be_bago.servici2')] )]" />
+	random=$((( RANDOM % 4 ) + 1 ))
+	random2=$((( RANDOM % 5 ) + 1 ))
+	random3=$((( RANDOM % 1000) + 20 ))	
+	random4=$((( RANDOM % 6 ) + 0 ))
+	random5=$((( RANDOM % 5 ) + 1 ))
+		echo '
+		<record model="hotels_be_bago.habitacion" id="habitacion'$i'">
+			<field name="hotel" ref="hotels_be_bago.hotel'$random'"></field>
+			<field name="name">Habitacion Number '$i'</field>
+			<field name="camas">'$random2'</field>
+			<field name="precios">'$random3'</field>
+			<field name="descripcion">Una '${arrayString[$random4]}' habitación presidencial. Perfecta para descansar y hacer todo tipo de travesuras.</field>
+<field name="fotos" eval="[(6,0,[ref('"'hotels_be_bago.roomfoto$random5'"')] )]" />
+			
 		</record>
-		";
+		'
 
 	i=$(( $i + 1 ))
 	done
-echo "
-		</record>
+echo '
+		
 	</data>
-</odoo>"
+</odoo>'
